e95b66c81035fc90902941751ffadec5b1c43457
Sam Thorold <sam.thorold@gmail.com>
2024-10-06 11:10:15 +0100

    Repository - action split init

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,15 +1,14 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { z } from "zod";
 
-// import { db } from "./db";
-// import { ingredients, meals } from "./db/schema";
-import { newMealSchema, createMeal } from "./repository";
+import {
+  newMealSchema,
+  createMeal,
+  newIngredientSchema,
+  createIngredient,
+} from "./repository";
 
-const ingredientSchema = z.object({
-  categoryId: z.number(),
-  name: z.string(),
-});
+// TODO: should auth be here? or userId bound to the action in the component?
 
 export async function createMealAction(formData: FormData) {
   const user = auth();
@@ -23,52 +22,17 @@ export async function createMealAction(formData: FormData) {
   redirect(`/meal/${newMeal.id}`);
 }
 
-export async function getMeal(mealId: number) {
-  const meal = await db.query.meals.findFirst({
-    where: (model, { eq }) => eq(model.id, mealId),
-  });
-  if (!meal) throw new Error(`Meal [${mealId}] not found`);
-  const ingredients = await db.query.ingredients.findMany({
-    where: (model, { eq }) => eq(model.mealId, mealId),
-  });
-  if (!ingredients) throw new Error(`No ingredients found`);
-  const ingredientsWithCategories = [];
-  for (const ingredient of ingredients) {
-    const maybeCategory = await db.query.categories.findFirst({
-      where: (model, { eq }) => eq(model.id, ingredient.categoryId),
-    });
-    ingredientsWithCategories.push({
-      ...ingredient,
-      categoryName: maybeCategory ? maybeCategory.name : "",
-    });
-  }
-  return { ...meal, ingredientsWithCategories };
-}
-
-export async function getCategories() {
-  const categories = await db.query.categories.findMany();
-  if (!categories) throw new Error("getCategories: No categories found");
-  return categories;
-}
-
-export async function createIngredient(mealId: number, formData: FormData) {
+export async function createIngredientAction(
+  mealId: number,
+  formData: FormData,
+) {
   const user = auth();
   if (!user.userId) throw new Error("Unauthorized");
   if (!formData) throw new Error("No body");
-  const rawFormData = {
-    categoryId: Number(formData.get("categoryId")),
-    name: formData.get("ingredientName"),
-  };
-  console.log(rawFormData);
-  const validatedData = ingredientSchema.parse(rawFormData);
-  console.log(validatedData);
-  const ingredient = await db
-    .insert(ingredients)
-    .values({
-      ...validatedData,
-      mealId,
-    })
-    .returning();
-  console.log(ingredient);
+  const validatedData = newIngredientSchema.parse({
+    ...formData.entries(),
+    mealId,
+  });
+  const newIngredient = await createIngredient(validatedData);
   redirect(`/meal/${mealId}`);
 }
--- src/lib/repository.ts
+++ src/lib/repository.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 import { db } from "./db";
-import { meals } from "./db/schema";
+import { ingredients, meals } from "./db/schema";
 
 export const newMealSchema = z.object({
   userId: z.string().min(1, "User ID cannot be empty"),
@@ -10,16 +10,61 @@ export const newMealSchema = z.object({
   mealName: z.string(),
 });
 
+export const newIngredientSchema = z.object({
+  mealId: z.number(),
+  categoryId: z.string().min(1, "Category ID cannot be empty"),
+  name: z.string().min(1, "Name cannot be empty"),
+});
+
 type NewMeal = z.infer<typeof newMealSchema>;
+type NewIngredient = z.infer<typeof newIngredientSchema>;
 
 export async function createMeal(newMeal: NewMeal) {
+  // TODO: do coersion with schema .transform
   const data = {
-    userId: newMeal.userId,
-    mealType: newMeal.mealType,
+    ...newMeal,
     mealTime: new Date(newMeal.mealTime),
-    mealName: newMeal.mealName,
   };
   const newRecords = await db.insert(meals).values(data).returning();
   if (!newRecords[0]) throw new Error("Error creating new meal.");
   return newRecords[0];
+}
+
+export async function createIngredient(newIngredient: NewIngredient) {
+  // TODO: do coersion with schema .transform
+  const data = {
+    ...newIngredient,
+    categoryId: Number(newIngredient.categoryId),
+  };
+  const newRecords = await db.insert(ingredients).values(data).returning();
+  if (!newRecords[0]) throw new Error("Error creating new ingredient.");
+  return newRecords[0];
+}
+
+export async function getMeal(mealId: number) {
+  const meal = await db.query.meals.findFirst({
+    where: (model, { eq }) => eq(model.id, mealId),
+  });
+  if (!meal) throw new Error(`Meal [${mealId}] not found`);
+  const ingredients = await db.query.ingredients.findMany({
+    where: (model, { eq }) => eq(model.mealId, mealId),
+  });
+  if (!ingredients) throw new Error(`No ingredients found`);
+  const ingredientsWithCategories = [];
+  for (const ingredient of ingredients) {
+    const maybeCategory = await db.query.categories.findFirst({
+      where: (model, { eq }) => eq(model.id, ingredient.categoryId),
+    });
+    ingredientsWithCategories.push({
+      ...ingredient,
+      categoryName: maybeCategory ? maybeCategory.name : "",
+    });
+  }
+  return { ...meal, ingredientsWithCategories };
+}
+
+export async function getCategories() {
+  const categories = await db.query.categories.findMany();
+  if (!categories) throw new Error("getCategories: No categories found");
+  return categories;
 }
